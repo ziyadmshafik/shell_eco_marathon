15078e9c842cd61f8fc76a0916d0c745cbfbf36b
ziyadmshafik <ziyadmshafik@users.noreply.github.com>
2017-02-22 14:35:44 +0300

    Update main_b.py

--- main_b.py
+++ main_b.py
@@ -64,3 +64,88 @@ GPIO.output(rightsignalpin, GPIO.LOW)
 
 GPIO.setwarnings(False)
 GPIO.setup(sensor,GPIO.IN,GPIO.PUD_UP)
+
+# Define some helper functions:
+class DummyClass: pass
+
+class Gauge(Widget):
+    '''
+    Gauge class
+    '''
+
+    dummy = DummyClass
+    unit = NumericProperty(1.8)
+    value = BoundedNumericProperty(0, min=0, max=100, errorvalue=0)
+    mypath = os.path.dirname(os.path.abspath(inspect.getsourcefile(dummy)))
+    file_gauge = StringProperty(mypath + os.sep + "cadran.png")
+    file_needle = StringProperty(mypath + os.sep + "needle.png")
+    size_gauge = BoundedNumericProperty(128, min=128, max=256, errorvalue=128)
+    size_text = NumericProperty(10)
+
+    def __init__(self, **kwargs):
+        super(Gauge, self).__init__(**kwargs)
+
+
+        self._gauge = Scatter(
+            size=(self.size_gauge, self.size_gauge),
+            do_rotate=False,
+            do_scale=False,
+            do_translation=False,
+	    scale=1.6
+            )
+
+        _img_gauge = Image(source=self.file_gauge, size=(self.size_gauge,
+            self.size_gauge))
+
+        self._needle = Scatter(
+            size=(self.size_gauge, self.size_gauge),
+            do_rotate=False,
+            do_scale=False,
+            do_translation=False,
+	    scale=1.6
+            )
+
+        _img_needle = Image(source=self.file_needle, size=(self.size_gauge,
+            self.size_gauge))
+
+        self._glab = Label(font_size=self.size_text, markup=True)
+        self._progress = ProgressBar(max=100, height=100, value=self.value)
+
+        self._gauge.add_widget(_img_gauge)
+        self._needle.add_widget(_img_needle)
+
+        self.add_widget(self._gauge)
+        self.add_widget(self._needle)
+        self.add_widget(self._glab)
+        self.add_widget(self._progress)
+
+        self.bind(pos=self._update)
+        self.bind(size=self._update)
+        self.bind(value=self._turn)
+
+    def _update(self, *args):
+        '''
+        Update gauge and needle positions after sizing or positioning.
+        '''
+        self._gauge.pos = self.pos
+        self._needle.pos = (self.x, self.y)
+        self._needle.center = self._gauge.center
+        self._glab.center_x = self._gauge.center_x
+        self._glab.center_y = self._gauge.center_y + (self.size_gauge/4)
+        self._progress.x = self._gauge.x+75
+        self._progress.y = self._gauge.y + (self.size_gauge / 4)
+        self._progress.width = self.size_gauge
+
+    def _turn(self, *args):
+        '''
+        Turn needle, 1 degree = 1 unit, 0 degree point start on 50 value.
+        '''
+        self._needle.center_x = self._gauge.center_x
+        self._needle.center_y = self._gauge.center_y
+        self._needle.rotation = (50 * self.unit) - (self.value * self.unit)
+        self._glab.text = "[b]{0:.0f}[/b]".format(self.value)
+        self._progress.value = self.value
+
+
+dirflag = 1
+value = 0

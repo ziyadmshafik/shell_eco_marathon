a15d4d58de68f27ecfb18de6e0b60444dd12324c
ziyadmshafik <ziyadmshafik@users.noreply.github.com>
2017-02-22 17:16:49 +0300

    Update main_b.py

--- main_b.py
+++ main_b.py
@@ -147,112 +147,6 @@ class Gauge(Widget):
         self._progress.value = self.value
 
 
-class StopWatch(Widget):
-
-    #gloabl variables
-    seconds = 0
-    minuets = 0
-    mili = 0
-    _mili = 0
-    time_track = StringProperty("00:00:00")
-    laps = []
-    running = False
-    resetting = True
-    addLap = False
-    timelbl = ObjectProperty(None)    
-    laplbl = ObjectProperty(None)
-    
-    def printLaps(self):
-        lap_string = ""
-        count = len(self.laps)
-        for lap in reversed(self.laps):
-            lap_string += "Lap " + str(count) +": " + lap + "\n"
-            count -=1
-        return lap_string   
-
-    def update(self,dt):
-        self.timelbl.text = str(self.time_track)
-        
-        if not self.laps:
-            self.laplbl.text = ""
-        else:
-            self.laplbl.text = self.printLaps()
-       
-        if(self.running):
-
-            if(self.seconds < 10 and self.minuets < 10 and self.mili < 10):
-                self.time_track = "0" + str(self.minuets) + ":0" + str(self.seconds) + ":0" + str(self.mili)
-            
-            elif(self.seconds < 10 and self.minuets < 10 and self.mili >= 10):
-                self.time_track = "0" + str(self.minuets) + ":0" + str(self.seconds) + ":" + str(self.mili)
-            
-            elif(self.seconds >= 10 and self.minuets < 10 and self.mili < 10 ):
-                self.time_track = "0" + str(self.minuets) + ":" + str(self.seconds) + ":0" + str(self.mili)
-
-            elif(self.seconds >= 10 and self.minuets < 10 and self.mili >= 10):
-                self.time_track = "0" + str(self.minuets) + ":" + str(self.seconds) + ":" + str(self.mili)
-
-            elif(self.seconds >= 10 and self.minuets >= 10 and self.mili < 10):
-                self.time_track =  str(self.minuets) + ":" + str(self.seconds) + ":0" + str(self.mili)
-
-            elif(self.seconds < 10 and self.minuets >= 10 and self.mili >= 10):
-                self.time_track = "0" + str(self.minuets) + ":" + str(self.seconds) + ":" + str(self.mili)
-            
-            elif(self.seconds < 10 and self.minuets >= 10 and self.mili < 10):
-                self.time_track = str(self.minuets) + ":0" + str(self.seconds) + ":0" + str(self.mili)
-            
-            elif(self.seconds >= 10 and self.minuets >= 10 and self.mili >= 10):
-                self.time_track = str(self.minuets) + ":" + str(self.seconds) + ":" + str(self.mili)
-
-            if(self.seconds >= 60):
-
-                self.minuets += 1
-                self.seconds = 0
-
-            if(self.mili >= 99):
-                self.seconds+=1
-                self.mili = 0
-                self._mili = 0
-
-            self._mili += dt*100
-            self.mili = int(round(self._mili, 0))
-           
-        if(self.resetting == True):
-            self.minuets = 0
-            self.seconds = 0
-            self.mili = 0
-            self.time_track = str(self.minuets) + "0:0" + str(self.seconds) + ":0" + str(self.mili)
-            self.laps = []
-            self.resetting = False
-            self.running = False
-
-        if(self.addLap == True):
-            self.laps.append(self.time_track)
-            self.laplbl.text = str(self.time_track)
-            self.addLap = False
-           
-
-    #Starts the timer
-    def start_time(self):
-        print("START TIME!")
-        self.running = True
-        
-    #Stops the timer
-    def stopTime(self):
-        print("STOP TIME!")
-        self.running = False
-
-    #Reset the timer
-    def resetTime(self):
-        print("RESET TIME!")
-        self.running = False
-        self.resetting = True
-
-    def Lap(self):
-        print("LAP!")
-        self.addLap = True
-        print(self.time_track)
-	
 dirflag = 1
 value = 0
 
@@ -402,9 +296,6 @@ class MyApp(App):
 		Clock.schedule_interval(update_speed, 0.5)
 		Clock.schedule_once(incgauge,1)
 		
-		stopwatchwidget = StopWatch();
-
-
 	# Create the rest of the UI objects (and bind them to callbacks, if necessary):
                 highbeam_button = ToggleButton(text="Highbeam\nLights", halign='center', size_hint=(.14,.14*2),pos = (2,20))
                 highbeam_button.bind(on_press=press_callback)
@@ -433,10 +324,8 @@ class MyApp(App):
                 layout.add_widget(wiper_button)
                 layout.add_widget(horn_button)
 #	        layout.add_widget(mygauge)
-                layout.add_widget(stopwatchwidget)
 
 #	        Clock.schedule_once(incgauge,5)
-		Clock.schedule_interval(stopwatchwidget.update, 1/100)
         	return layout
 
 if __name__ == '__main__':
